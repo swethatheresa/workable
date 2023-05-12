f737aa11a2b6d4609b6f368d0221551c63fc290d
swethatheresa <swethatheresa123@gmail.com>
2023-05-13 00:38:01 +0530

    modified the theme settings

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,7 +1,25 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  // Your theme settings
+  palette: {
+    primary: {
+      main: '#3AAE45',
+      contrastText: '#F9F9F9',
+    },
+    secondary: {
+      main: '#CAFFCF',
+      contrastText: '#353535',
+    },
+  },
+    typography: {
+      fontFamily: 'Inter, Roboto, sans-serif',
+      heading1: {
+        color: '#353535',
+      },
+      heading2: {
+        color: '#767676',
+      },
+  },
 });
 
 export default theme;
