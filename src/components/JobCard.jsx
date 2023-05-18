60c413c7aadd8ef7d8f03a2053eb5bc1acabdfd6
deviibinoy <79354653+deviibinoy@users.noreply.github.com>
2023-05-18 18:57:37 +0530

    Update Responsive JobCard component

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardContent, Typography, Box, Chip } from '@mui/material';
+import React from "react";
+import { Card, CardContent, Typography, Box, Chip, useTheme } from "@mui/material";
 
 const JobCard = ({
   companyLogo,
@@ -9,58 +9,125 @@ const JobCard = ({
   jobTypes,
   applicationStatus,
 }) => {
+  const theme = useTheme();
+
   return (
     <Card
       sx={{
-        // width: '100%',
-        maxWidth: 961,
-        height: 'auto',
-        borderRadius: '10px',
-        marginBottom: '1.5em',
-        marginLeft: { xs: '5%', sm: '15%' },
+        margin: "1.5rem auto",
+        borderRadius: "10px",
+        [theme.breakpoints.up("sm")]: {
+          maxWidth: "70%",
+        },
+        position: "relative", 
       }}
     >
       <CardContent>
         <Box
           display="flex"
-          flexDirection={{ xs: 'column', sm: 'row' }}
+          flexDirection={{ xs: "column", md: "row", sm: "row" }}
           alignItems="center"
-          justifyContent={{ xs: 'center', sm: 'flex-start' }}
+          justifyContent={{ xs: "center", sm: "flex-start" }}
         >
           <img
             src={companyLogo}
             alt="Company Logo"
             style={{
-              width: { xs: 80, sm: 120 },
-              height: { xs: 80, sm: 120 },
-              marginRight: 16,
-              alignSelf: 'center',
+              width: "5rem",
+              height: "5rem",
+              marginRight: "1rem",
+              alignSelf: "center",
             }}
           />
-          <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+          <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
             <Box
               display="flex"
               justifyContent="space-between"
               alignItems="center"
-              flexDirection={{ xs: 'column', sm: 'row' }}
+              flexDirection={{ xs: "column", sm: "row" }}
             >
-              <Typography variant="h6" style={{ marginBottom: 0, textAlign: 'left' }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontSize: "1.3rem",
+                  fontWeight: "600",
+                  [theme.breakpoints.down("md")]: {
+                    fontSize: "1rem",
+                  },
+                  [theme.breakpoints.down("sm")]: {
+                    fontSize: "0.9rem",
+                  },
+                  [theme.breakpoints.down("xs")]: {
+                    fontSize: "0.7rem",
+                  },
+                }}
+              >
                 {title}
               </Typography>
-              <Chip
-                label={applicationStatus}
-                variant="outlined"
-                style={{ backgroundColor: '#E3E9FF' }}
-              />
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "0.5rem",
+                  right: "0.5rem",
+                  [theme.breakpoints.down("md")]: {
+                    top: "0.3rem",
+                    right: "0.3rem",
+                  },
+                  [theme.breakpoints.down("sm")]: {
+                    top: "0.2rem",
+                    right: "0.2rem",
+                  },
+                  [theme.breakpoints.down("xs")]: {
+                    top: "0.2rem",
+                    right: "0.2rem",
+                  },
+                }}
+              >
+                <Chip
+                  label={applicationStatus}
+                  variant="outlined"
+                  sx={{
+                    backgroundColor: "#E3E9FF",
+                    color: "#4668E0",
+                    fontSize: "0.9rem",
+                    marginTop: "0.9rem",
+                    [theme.breakpoints.down("md")]: {
+                      fontSize: "0.7rem",
+                      marginTop: "0.65rem",
+                      height: "0.8rem"
+                    },
+                    [theme.breakpoints.down("sm")]: {
+                      fontSize: "0.6rem",
+                      height: "0.7rem"
+                    },
+                    [theme.breakpoints.down("xs")]: {
+                      fontSize: "0.5rem",
+                      height: "0.9rem"
+                    },
+                  }}
+                />
+              </Box>
             </Box>
             <Typography
               variant="body1"
-              style={{
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                display: '-webkit-box',
-                WebkitLineClamp: 1,
-                WebkitBoxOrient: 'vertical',
+              sx={{
+                fontSize: "1rem",
+                maxWidth: "35%",
+                mt: "0.5rem",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                [theme.breakpoints.down("md")]: {
+                  fontSize: "0.9rem",
+                },
+                [theme.breakpoints.down("sm")]: {
+                  fontSize: "0.8rem",
+                  maxWidth: "300px",
+                },
+                [theme.breakpoints.down("xs")]: {
+                  fontSize: "0.7rem",
+                  maxWidth: "200px",
+                },
               }}
             >
               {description}
@@ -71,20 +138,62 @@ const JobCard = ({
                   key={index}
                   label={disability}
                   sx={{
-                    marginRight: 1,
-                    backgroundColor: '#CAFFCF',
+                    marginRight: "0.5rem",
+                    backgroundColor: "#CAFFCF",
                     borderRadius: 3,
-                    height: '2.5em',
+                    [theme.breakpoints.down("md")]: {
+                      height: "1rem",
+                      fontSize: "0.9rem",
+                    },
+                    [theme.breakpoints.down("sm")]: {
+                      height: "0.9rem",
+                      fontSize: "0.7rem",
+                      marginLeft: "0.6rem",
+                    },
+                    [theme.breakpoints.down("xs")]: {
+                      height: "0.7rem",
+                      fontSize: "0.6rem",
+                      marginLeft: "0rem",
+                    },
                   }}
                 />
               ))}
             </Box>
-            <Box display="flex" alignItems="center" marginTop={1}>
-              <Typography variant="body2" style={{ color: '#767676' }}>
-                {jobTypes.join(' | ')}
+            <Box
+              display="flex"
+              alignItems="center"
+              marginTop={1}
+              sx={{
+                [theme.breakpoints.down("md")]: {
+                  marginLeft: "0.1rem",
+                },
+                [theme.breakpoints.down("sm")]: {
+                  marginLeft: "3rem",
+                },
+                [theme.breakpoints.down("xs")]: {
+                  marginLeft: "0.1rem",
+                },
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#767676",
+                  [theme.breakpoints.down("md")]: {
+                    fontSize: "0.9rem",
+                  },
+                  [theme.breakpoints.down("sm")]: {
+                    fontSize: "0.75rem",
+                  },
+                  [theme.breakpoints.down("xs")]: {
+                    fontSize: "0.6rem",
+                  },
+                }}
+              >
+                {jobTypes.join(" | ")}
               </Typography>
             </Box>
-          </div>
+          </Box>
         </Box>
       </CardContent>
     </Card>
