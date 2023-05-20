630f0b4529948e8af81efebfb62597d936e8e93b
deviibinoy <79354653+deviibinoy@users.noreply.github.com>
2023-05-20 09:27:43 +0530

    company details form updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import JobPost from './components/JobPost'
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme.jsx';
-import EmployerLogin from './components/EmployerLogin'
-import JobCard from './components/JobCard'
-import MyJobs from './components/MyJobs';
-import { BrowserRouter as Router,Route,Routes,Link } from 'react-router-dom/dist';
-import Applyform from './components/Applyform';
+import { useState } from "react";
+import "./App.css";
+import JobPost from "./components/JobPost";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme.jsx";
+import EmployerLogin from "./components/EmployerLogin";
+import MyJobs from "./components/MyJobs";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom/dist";
+import Applyform from "./components/Applyform";
+import CompanyForm from "./components/CompanyForm";
 function App() {
-  const [count, setCount] = useState(0)
-  return (<>
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Router>
-      <Routes>
-        <Route path="/" element={<EmployerLogin/>} />
-        <Route path="/jobpost" element={<JobPost/>} />
-        <Route path="/myjobs" element={<MyJobs/>} />
-        <Route path="/applyform" element={<Applyform/>} />
-      </Routes>
-    </Router>
-    </ThemeProvider>
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<EmployerLogin />} />
+            <Route path="/jobpost" element={<JobPost />} />
+            <Route path="/myjobs" element={<MyJobs />} />
+            <Route path="/applyform" element={<Applyform />} />
+            <Route path="/companyform" element={<CompanyForm />} />
+          </Routes>
+        </Router>
+        <CompanyForm />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
