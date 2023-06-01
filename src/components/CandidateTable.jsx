5035fdbc98f9ce0297bbb3447866261b3b6753cc
swethatheresa <swethatheresa123@gmail.com>
2023-06-01 22:28:40 +0530

    added redux toolkit store

--- src/components/CandidateTable.jsx
+++ src/components/CandidateTable.jsx
@@ -47,7 +47,7 @@ const CandidateTable = () => {
        >
         <TabContext value={value} flexDirection={'column'}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <TabList onChange={handleChange} aria-label="Application status of candidates">
                 <Tab label="All" value="1" />
                 <Tab label="Accepted" value="2" />
                 <Tab label="Rejected" value="3" />
