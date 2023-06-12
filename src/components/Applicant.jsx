2bf21478ea28daf908c7e8ff42b685eba78ffd95
swethatheresa <swethatheresa123@gmail.com>
2023-06-12 18:54:15 +0530

    added fetch applicant functions

--- src/components/Applicant.jsx
+++ src/components/Applicant.jsx
@@ -1,138 +1,120 @@
-import React from 'react'
-import Grid from '@mui/material/Unstable_Grid2'; 
-import { Button, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
+import { Button, Typography, Link, CircularProgress} from '@mui/material';
 import theme from '../theme';
-
+import { fetchApplicant } from '../services/Applicants';
 
 const Applicant = () => {
+  const [applicant, setApplicant] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchApplicant('ZLGMUmrp5fnD7LnWPFdF').then((res) => {
+      setApplicant(res);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading || !applicant) {
+    return (
+        <Grid container alignItems={'center'} justifyContent={'center'} height={'50%'} width={'50%'}>
+          <CircularProgress size={40}/>
+        </Grid>
+    )
+  }
+
   return (
-    <Grid container spacing={3} direction={'column'} wrap={'wrap'}
+
+    <Grid
+      container
+      spacing={3}
+      direction="column"
+      wrap="wrap"
+      sx={{
+        backgroundColor: '#FFFFFF',
+        boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.05)',
+        borderRadius: '9px',
+        m: '1.5em',
+        p: '2rem',
+        width: '65vw',
+        [theme.breakpoints.down('md')]: {
+          width: '90vw',
+          m: '1em',
+          p: '1rem',
+        },
+      }}
+    >
+      <Grid container direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="heading1" sx={{ fontWeight: 'bold' }}>
+          {applicant.name}
+        </Typography>
+        <Button variant="contained" sx={{ fontSize: '1em', width: '8em', borderRadius: '10px' }}>
+          Shortlist
+        </Button>
+      </Grid>
+      <Typography variant="heading2">Applied on {applicant.appliedDate.toDate().toDateString()}</Typography>
+      <Typography variant="heading1" sx={{ mt: '1em', fontWeight: 'bold', fontSize: '1.1em' }}>
+        Contact Number
+      </Typography>
+      <Typography variant="heading2">{applicant.contactNumber}</Typography>
+      <Typography variant="heading1" sx={{ fontWeight: 'bold', fontSize: '1.1em' }}>
+        Email
+      </Typography>
+      <Typography variant="heading2">{applicant.email}</Typography>
+      <Typography variant="heading1" sx={{ fontWeight: 'bold', fontSize: '1.1em' }}>
+        Address
+      </Typography>
+      <Typography variant="heading2">{applicant.address}</Typography>
+      <Typography variant="heading1" sx={{ mb: '-0.5em', fontWeight: 'bold', fontSize: '1.1em' }}>
+        Disability
+      </Typography>
+      <Grid
+        container
+        xs={12}
         sx={{
-            backgroundColor: '#FFFFFF',
-            boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.05)',
-            borderRadius: '9px',
-            m : '1.5em',
-            p: '2rem',
-            width: '65vw',
-            [theme.breakpoints.down('md')]: {
-                width: '90vw',
-                m : '1em',
-                p: '1rem',
-            },
+          flexDirection: 'row',
+          flexWrap: 'wrap',
         }}
       >
-        <Grid container direction="row" justifyContent="space-between" alignItems="center">
-            <Typography variant='heading1' sx={{ fontWeight:'bold'}} >
-                Charlie Keaton
-            </Typography>
-            <Button variant='contained' sx={{ fontSize: '1em',width:'8em',borderRadius:'10px'}}>
-                Shortlist
-            </Button>
-        </Grid>
-        <Typography variant='heading1' 
-            sx={{ 
-                mt: '1em',
-                fontWeight:'bold', 
-                fontSize: '1.1em'
-            }} >
-            Contact Number
-        </Typography>
-        <Typography variant='heading2' >
-            123-456-7890
-        </Typography>
-        <Typography variant='heading1' sx={{ fontWeight:'bold', fontSize: '1.1em'}} >
-            Email
-        </Typography>
-        <Typography variant='heading2' >
-            charlie@gmail.com
-        </Typography>
-        <Typography variant='heading1'  sx={{ fontWeight:'bold', fontSize: '1.1em'}} >
-            Address
-        </Typography>
-        <Typography variant='heading2' >
-            1234 Main Street, Anytown, USA
-        </Typography>
-        <Typography variant='heading1' sx={{ mb: '-0.5em',fontWeight:'bold', fontSize: '1.1em'}} >
-            Disability
-        </Typography>
-        <Grid container xs={12}  sx={{
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-            }}>
-                <Typography sx={{
-                    backgroundColor: 'secondary.main',
-                    pl: '1rem',
-                    pr: '1rem',
-                    pt: '0.3rem',
-                    pb: '0.3rem',
-                    mr: '1rem',
-                    mt: '1rem',
-                    borderRadius: '5px',
-                    fontWeight: 'medium',
-                    [theme.breakpoints.down('sm')]: {
-                        fontSize: '0.8rem',
-                    },
-                }}>
-                    Hearing Impairment
-                </Typography>
-                <Typography sx={{
-                    backgroundColor: 'secondary.main',
-                    pl: '1rem',
-                    pr: '1rem',
-                    pt: '0.3rem',
-                    pb: '0.3rem',
-                    mr: '1rem',
-                    mt: '1rem',
-                    fontWeight: 'medium',
-                    borderRadius: '5px',
-                    [theme.breakpoints.down('sm')]: {
-                        fontSize: '0.8rem',
-                    },
-                }}>
-                    Speech Impairment
-                </Typography>
-                <Typography sx={{
-                    backgroundColor: 'secondary.main',
-                    pl: '1rem',
-                    pr: '1rem',
-                    pt: '0.3rem',
-                    pb: '0.3rem',
-                    mr: '1rem',
-                    mt: '1rem',
-                    fontWeight: 'medium',
-                    borderRadius: '5px',
-                    [theme.breakpoints.down('sm')]: {
-                        fontSize: '0.8rem',
-                    },
-                }}>
-                    Learning Disability
-                </Typography>
-        </Grid>  
-        <Typography variant='heading1' sx={{ mt:'1rem',fontWeight:'bold', fontSize: '1.1em'}} >
-            Qualification Level
-        </Typography>
-        <Typography variant='heading2' >
-            Bachelor's Degree
-        </Typography>
-        <Typography variant='heading1' sx={{ fontWeight:'bold', fontSize: '1.1em'}} >
-            Experience Level
-        </Typography>
-        <Typography variant='heading2' >
-            2 years
-        </Typography>
-        <Typography variant='heading1' sx={{ fontWeight:'bold', fontSize: '1.1em'}} >
-            Cover Letter
-        </Typography>
-        <Typography variant='heading2' >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
-            book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-            with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </Typography>
+        {applicant.disabilityCategory && Array.isArray(applicant.disabilityCategory) && applicant.disabilityCategory.map((disability, index) => (
+          <Typography
+            key={index}
+            sx={{
+              backgroundColor: 'secondary.main',
+              pl: '1rem',
+              pr: '1rem',
+              pt: '0.3rem',
+              pb: '0.3rem',
+              mr: '1rem',
+              mt: '1rem',
+              fontWeight: 'medium',
+              borderRadius: '5px',
+              [theme.breakpoints.down('sm')]: {
+                fontSize: '0.8rem',
+              },
+            }}
+          >
+            {disability}
+          </Typography>
+        ))}
+      </Grid>
+      <Typography variant="heading1" sx={{ mt: '1rem', fontWeight: 'bold', fontSize: '1.1em' }}>
+        Qualification Level
+      </Typography>
+      <Typography variant="heading2">{applicant.qualification}</Typography>
+      <Typography variant="heading1" sx={{ fontWeight: 'bold', fontSize: '1.1em' }}>
+        Experience Level
+      </Typography>
+      <Typography variant="heading2">{applicant.experience}</Typography>
+      <Typography variant="heading1" sx={{ fontWeight: 'bold', fontSize: '1.1em' }}>
+        Resume Link
+      </Typography>
+      <Link rel="noopener noreferrer" href={applicant.resumeLink} target="_blank">
+      <Typography variant="heading2">{applicant.resumeLink}</Typography>
+      </Link>
     </Grid>
-        
-  )
-}
+  );
+};
 
-export default Applicant
+export default Applicant;
--- src/services/Applicants.js
+++ src/services/Applicants.js
@@ -1,3 +1,25 @@
-import { collection} from 'firebase/firestore';
+import { collection, orderBy, getDocs, query, where, getDoc , doc} from 'firebase/firestore';
 import { db } from "../firebase";
-const ref = collection(db, 'joblistings'); 
+const ref = collection(db, 'applicants'); 
+
+export const fetchApplicants = async (jobid) => {
+    const querySnapshot = await getDocs(query(ref,orderBy("appliedDate","desc"),where("joblistingId", "==", jobid)))
+    const applicants = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    return applicants;
+}
+
+export const fetchApplicant = async (id) => {
+    const docRef = await getDoc(doc(db, 'applicants', id));
+    if (docRef.exists()) {
+      const applicant = {
+        id: docRef.id,
+        ...docRef.data()
+      };
+      return applicant;
+    }
+    return null; 
+  };
+  
